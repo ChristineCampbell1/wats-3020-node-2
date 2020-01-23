616002df2308b1d1fefe8b05c408ad71e042c3c2
Christine Campbell <ccamp67@gmail.com>
2020-01-22 21:19:46 -0800

    node 5-character-count with a section on line 19 not working

--- 5-character-count/index.js
+++ 5-character-count/index.js
@@ -1,9 +1,9 @@
 //TODO fill in comment template
 /*
-Description:
-Input:
-Output:
-Usage: 
+Description: a program that takes a string as input and counts how many of each character are in that string.
+Input: a string
+Output:character count
+Usage: node 5-character-count
 */
 
 // get input using getargs
@@ -12,26 +12,35 @@ let input = getargs.getStringArg()
 
 
 //test that it is a string
-//TODO check for string argument and if no string is entered provide a usage statement and quit
-if ( ) {
-  // TODO provide a usage statement
+if (!input || input.length == 0);
+console.log(`usage: node 5-character-count <string>`);
+
+  let charCount = {};
+  let characters = input.split('');
+
+  for (let character of characters){
+    if (charCount[character]){
+      charCount[character]++;
+    } else {
+      charCount[character] = 1;
+    }
 }
-else {
-  // TODO initialize charCount object to count characters
-  // TODO turn the input into an array called characters using the split method
+for (let character in charCount) {
+  console.log(`${character} ${charCount}`);
   
-  // TODO iterate through array to create object that has character for key and count for value
-  
-  // TODO initialize a new array called charArr
-  // TODO loop through every character in charCount 
-  for ( ) {
+let charArr = [];
+for (let character in charCount){
     let newElem = {char:character, count:charCount[character]};
-    // TODO add newElem to charArr using the push method
+    charArr.push(newElem);
   }
 
-  // TODO use the sort method of charArr with a custom compare function to sort the array by the count values (least to greatest)
+charArr.sort(function(a,b){
+    return a.count-b.count;
+});
 
-  // TODO output the content of the array to the console, showing each character and the count
+for (let row of charArr){
+  console.log(`${row.char} ${row.count}`);
+  }
   
 }
 
